bf4cba93e2a2e53871acd2ae73d1231ad4e3e495
DimaSurdiy <dsurdiy@ukr.net>
2022-08-13 15:03:23 +0300

    ContactForm component updated with hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,76 +1,65 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
 import { IoMdContact } from 'react-icons/io';
 import { BsFillPhoneFill } from 'react-icons/bs';
 import { Form, FormInput, AddContactBtn } from './ContactForm.styled';
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = e => {
+    const newName = e.target.value;
+    setName(newName);
   };
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-
-    this.setState({
-      [name]: value,
-    });
+  const handleNumberChange = e => {
+    const newNumber = e.target.value;
+    setNumber(newNumber);
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    const newContact = this.createContact();
+    const newContact = { id: nanoid(), name, number };
+    onSubmit(newContact);
 
-    this.props.onSubmit(newContact);
-    this.reset();
+    setName('');
+    setNumber('');
   };
 
-  createContact = () => {
-    const { name, number } = this.state;
-    return {
-      id: nanoid(),
-      name,
-      number,
-    };
-  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <label>
+        <IoMdContact size={20} fill="tomato" /> Name
+        <FormInput
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label>
+        <BsFillPhoneFill size={18} fill="tomato" /> Number
+        <FormInput
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleNumberChange}
+        />
+      </label>
+      <AddContactBtn type="submit">Add contact</AddContactBtn>
+    </Form>
+  );
+};
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <label>
-          <IoMdContact size={20} fill="tomato" /> Name
-          <FormInput
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          <BsFillPhoneFill size={18} fill="tomato" /> Number
-          <FormInput
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.handleChange}
-          />
-        </label>
-        <AddContactBtn type="submit">Add contact</AddContactBtn>
-      </Form>
-    );
-  }
-}
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
